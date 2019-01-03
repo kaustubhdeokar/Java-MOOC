64db5972f2285a73ec31afcfab1618c51ab0a16b
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-01-03 23:16:01 +0530

    JFrame basics

--- Basics_JFrame.java
+++ Basics_JFrame.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
 
-public class Calculator {
+public class Basics_JFrame {
 	
 	public static void main(String[] args){
 		
 		JFrame frame=new JFrame();
+		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame.setVisible(true);
 		frame.setSize(200, 200);
 		frame.setLocation(200,200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//or both the above can we merged together//setBounds(setLocation-x,setLocation-y,setsize-x,setsize-y) 
-		
+//or both the above can we merged together//setBounds(setLocation-x,setLocation-y,setsize-x,setsize-y) 
 		frame.setTitle("Calc");
+		ImageIcon obj=new ImageIcon("/icons/icons1.jpg");
+		frame.setIconImage(obj.getImage());
 	}
 	
 }
