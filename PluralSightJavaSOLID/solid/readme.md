bb6500318b0a9a66ec068d7f1218f9bd051a838f
kaustubhdeokar <kaustubhd9@gmail.com>
2021-04-26 23:22:23 +0530

    Lambdas-Consumer/Supplier

---
+++ PluralSightJavaSOLID/solid/readme.md
@@ -0,0 +1,13 @@
+## SOLID Principles
+
+These exercises are part of the pluralsight course [SOLID Software Design Principles in Java](https://app.pluralsight.com/library/courses/solid-software-design-principles-java/table-of-contents)
+
+### Liskov's Substitution Principle
+
+> If S is a subtype of T, then objects of type T maybe replaced by objects of type S without modifying the functionality of the code. 
+
+> So by some additional requirements introduced, the derived type does not fulfill the above principle, then we can break the relationship. 
+
+
+### Interface Segregation Principle 
+
--- Streams/MainClass.java
+++ Streams/MainClass.java
@@ -1,6 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class MainClass {
 
+    public static void main(String[] args) {
 
+        List<Integer> integers = getPrimeNumbersTo50().get();
+
+        integers.forEach(System.out::println);
+
+        consumeString().accept("SomeString");
+
+
+    }
+
+    private static Consumer<String> consumeString() {
+        return (s) -> System.out.println(s+2);
+    }
+
+    private static Supplier<List<Integer>> getPrimeNumbersTo50() {
+        Supplier<List<Integer>> primeNumbersTo50 = () -> {
+            List<Integer> list = getListTo50().get();
+            for (int i = 2; i * i <= 50; i++) {
+                if (list.contains(i)) {
+                    for (int j = i * i; j <= 50; j += i) {
+                        list.remove(Integer.valueOf(j));
+                    }
+                }
+            }
+            return list;
+        };
+        return primeNumbersTo50;
+    }
+
+    private static Supplier<List<Integer>> getListTo50() {
+        Supplier<List<Integer>> listTo50 = () -> {
+            List<Integer> listIntegersUpto50 = new ArrayList<>();
+            for (int i = 2; i < 51; i++) {
+                listIntegersUpto50.add(i);
+            }
+            return listIntegersUpto50;
+        };
+        return listTo50;
+    }
 
 }
